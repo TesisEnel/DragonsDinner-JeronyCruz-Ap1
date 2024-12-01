543304c8d64d6e6758ea80b249c4b7d4180d4a4a
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-30 21:32:02 -0400

    Aplicando clean architecture

---
+++ DragonsDinner.Domain/DTO/TarjetasDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonsDinner.Domain.DTO;
+
+public class TarjetasDto
+{
+}
