68c12ba99c496e211210dfa4e681b1ef9e411619
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-30 21:33:39 -0400

    Cambiando visibilidad de servicios

--- DragonsDinner.Services/TarjetasService.cs
+++ DragonsDinner.Services/TarjetasService.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DragonsDinner.Services
+namespace DragonsDinner.Services;
+
+public class TarjetasService
 {
-    internal class TarjetasService
-    {
-    }
 }
--- DragonsDinner.Services/UsuariosService.cs
+++ DragonsDinner.Services/UsuariosService.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DragonsDinner.Services
+namespace DragonsDinner.Services;
+
+public class UsuariosService
 {
-    internal class UsuariosService
-    {
-    }
 }
