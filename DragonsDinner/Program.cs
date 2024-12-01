c11686e0d3384897772341ac8cf68ca2a27f94d4
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-30 20:57:12 -0400

    Modificando connetion string

--- DragonsDinner/Program.cs
+++ DragonsDinner/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("SqlConStr") ?? throw new InvalidOperationException("Connection string 'SqlConStr' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
