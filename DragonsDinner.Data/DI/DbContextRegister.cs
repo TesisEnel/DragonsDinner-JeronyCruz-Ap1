b585c441c1a20bb4b4266b562b1290821f546466
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-30 23:16:19 -0400

    Agregando DbContextRegister

---
+++ DragonsDinner.Data/DI/DbContextRegister.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonsDinner.Data.DI;
+
+public static class DbContextRegister
+{
+    public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
+    {
+        services.AddDbContextFactory<ApplicationDbContext>(o => o.UseSqlServer("Name=SqlConStr"));
+        return services;
+    }
+
+}
