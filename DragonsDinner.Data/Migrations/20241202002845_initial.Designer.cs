eaa8afaf7d2062612024c7e408dcd100153e5226
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-12-01 23:08:16 -0400

    Nueva migracion y actualización de la base de datos

--- DragonsDinner.Data/Migrations/20241202002845_initial.Designer.cs
+++ DragonsDinner.Data/Migrations/20241202002845_initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DragonsDinner.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241201194213_Initial")]
-    partial class Initial
+    [Migration("20241202002845_initial")]
+    partial class initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
