7c10eeff82de81e1c9e69294cbda3ba090acaaf1
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-12-05 22:49:47 -0400

    Creando relación entre direcciones y provincias

--- DragonsDinner.Data/Models/Direcciones.cs
+++ DragonsDinner.Data/Models/Direcciones.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,12 @@ public class Direcciones
 
     [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres.")]
     public string Nombre { get; set; }
-    public string Provincia { get; set; }
+
+    [ForeignKey("Provincia")]
+    [Range(1, int.MaxValue, ErrorMessage = "Por favor, seleccione una opción válida")]
+
+    public int ProvinciaId { get; set; }
+    public Provincias? Provincia { get; set; }
 
     [Required(ErrorMessage = "Debe ingresar un municipio")]
     [StringLength(50, ErrorMessage = "El municipio no puede tener más de 50 caracteres.")]
