b4005136f05cdb941f8e5f6675c787542396dc6b
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-12-07 20:51:43 -0400

    Mejoras en el UsuariosDto

--- DragonsDinner.Domain/DTO/UsuariosDto.cs
+++ DragonsDinner.Domain/DTO/UsuariosDto.cs
@@ -1,4 +1,5 @@
-﻿using DragonsDinner.Data.Models;
+﻿using DragonsDinner.Data;
+using DragonsDinner.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DragonsDinner.Domain.DTO;
 
-public class UsuariosDto
+public class UsuariosDto : ApplicationUser
 {
     public int UsuarioId { get; set; }
     public string Nombres { get; set; }
