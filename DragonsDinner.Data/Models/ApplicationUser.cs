0d21d606f76467cbe2ce5060e448f869565ce836
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-12-07 20:47:06 -0400

    Se agreagaron las Icollection Pertinentes

--- DragonsDinner.Data/Models/ApplicationUser.cs
+++ DragonsDinner.Data/Models/ApplicationUser.cs
@@ -1,8 +1,13 @@
+using DragonsDinner.Data.Models;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DragonsDinner.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public ICollection<Tarjetas> Tarjetas { get; set; } = new List<Tarjetas>();
+    public ICollection<Direcciones> Direcciones { get; set; } = new List<Direcciones>();
+
 }
