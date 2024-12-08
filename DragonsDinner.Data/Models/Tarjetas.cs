6a8c0810c9bdc3c52da3e5d65d6ec3219c471970
Jerony Ulises Cruz Fermin <jerony_cruz@ucne.edu.do>
2024-12-07 20:49:22 -0400

    Se agrego la Foreign key con ApplicationUsser

--- DragonsDinner.Data/Models/Tarjetas.cs
+++ DragonsDinner.Data/Models/Tarjetas.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,5 +29,9 @@ public class Tarjetas
     [Required(ErrorMessage = "El CVV es obligatorio.")]
     [RegularExpression(@"^\d{3,4}$", ErrorMessage = "El CVV debe contener 3 o 4 dígitos.")]
     public string CVV { get; set; }
+
+    [ForeignKey("Usuario")]
+    public string? UsuarioId { get; set; }
+    public ApplicationUser? Usuario { get; set; }
 }
 
